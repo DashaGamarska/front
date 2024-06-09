9bce93a867c2aea0641bb5ae543ec411b41680e1
DashaGamarska <108406951+DashaGamarska@users.noreply.github.com>
2024-06-09 16:13:13 +0300

    Update fetchBags.ts

--- lib/api-services/fetchBags.ts
+++ lib/api-services/fetchBags.ts
@@ -1,5 +1,5 @@
 import { BASE_URL } from '@components/constants';
-import { BagsDetailsI } from '@components/types';
+
 
 export const fetchBags = async (lang: ServerLocale): Promise<BagsDetailsI[]> => {
   //TODO: Remove revalidate
