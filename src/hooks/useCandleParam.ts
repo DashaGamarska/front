a05466ce4f9625ed665cd0f5f9bca552eef34f29
DashaGamarska <108406951+DashaGamarska@users.noreply.github.com>
2024-06-11 22:37:09 +0300

    Update useCandleParam.ts

--- src/hooks/useCandleParam.ts
+++ src/hooks/useCandleParam.ts
@@ -3,9 +3,7 @@ import { useState } from 'react';
 import { getInitParamCandle } from '@components/helpers';
 
 export const useCandleParam = () => {
-  const [paramCandle, setParamCandle] = useState<CustomCandleDescription>(
-    getInitParamCandle()
-  );
+
 
   const handleChangeCandleParam = (key: string, param: number) => {
     setParamCandle({ ...paramCandle, [key]: param });
