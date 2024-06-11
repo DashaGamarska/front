b9f7c8b44909567c775b740edbdaa6ee0229cede
DashaGamarska <108406951+DashaGamarska@users.noreply.github.com>
2024-06-11 22:30:53 +0300

    Update convertToServerLocale.ts

--- src/helpers/convertToServerLocale.ts
+++ src/helpers/convertToServerLocale.ts
@@ -1,5 +1,5 @@
 import type { Locale } from '@i18n';
-
+import {ServerLocale} from "@components/types";
 export const convertToServerLocale = (lang: Locale): ServerLocale => {
   return lang === 'uk' ? 'UA' : 'EN';
 };
