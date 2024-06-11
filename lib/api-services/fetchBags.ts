8b83d3ba0ce53af8f1a129bc728cf563bd1c58bf
DashaGamarska <108406951+DashaGamarska@users.noreply.github.com>
2024-06-11 23:08:54 +0300

    Update fetchBags.ts

--- lib/api-services/fetchBags.ts
+++ lib/api-services/fetchBags.ts
@@ -2,6 +2,19 @@ import { BASE_URL } from '@components/constants';
 import { BoxDetailsI  } from '@components/types';
 
 export type ServerLocale = 'UA' | 'EN';
+interface BoxDetailsI {
+  id: string;
+  images: string[];
+  title: string;
+  name: string;
+  price: number;
+  components: ComponentI[];
+  description: string;
+  slug: string;
+  volume: string;
+  text: string;
+  kit: string | undefined;
+}
 export const fetchBags = async (lang: ServerLocale): Promise<BoxDetailsI []> => {
   //TODO: Remove revalidate
   const response = await fetch(`${BASE_URL}/bags?lang=${lang}`, {next: { revalidate: 3600 },
