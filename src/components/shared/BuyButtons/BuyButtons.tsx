6562ba265af438d99561368a574b6c8de51f3567
DashaGamarska <108406951+DashaGamarska@users.noreply.github.com>
2024-06-11 13:34:01 +0300

    Update BuyButtons.tsx

--- src/components/shared/BuyButtons/BuyButtons.tsx
+++ src/components/shared/BuyButtons/BuyButtons.tsx
@@ -1,7 +1,10 @@
+// src/components/shared/BuyButtons/BuyButtons.tsx
+
 'use client';
 import React from 'react';
 import Button from '@components/components/Button/Button';
 import { useCartActionsContext } from '@context/CartContext';
+import { IToastMessages } from '@components/types';
 
 interface Product {
   id: string;
@@ -17,9 +20,11 @@ interface Product {
 interface BuyButtonsProps {
   product: Product;
   isBox: boolean;
+  quantity: number;  // Add quantity here
+  toastMessages: IToastMessages;  // Ensure this is passed as well
 }
 
-const BuyButtons: React.FC<BuyButtonsProps> = ({ product, isBox }) => {
+const BuyButtons: React.FC<BuyButtonsProps> = ({ product, isBox, quantity, toastMessages }) => {
   const { addBoxToCart } = useCartActionsContext();
   const { id, price, images, description, slug, name, title, aroma } = product;
 
@@ -32,8 +37,9 @@ const BuyButtons: React.FC<BuyButtonsProps> = ({ product, isBox }) => {
           onClick={() =>
             addBoxToCart({
               id,
+              toastMessage: toastMessages.itemAdded,
               aroma,
-              quantity: 1,
+              quantity,
               price,
               images,
               description,
